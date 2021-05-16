9a7ac8f7524fa9c17a9dcf8aef593785c4f26f5b
Sicelo24 <sicelozitha@gmail.com>
2021-05-16 21:12:46 +0200

    Latest update

--- src/main/java/za/ac/cput/assignment2/App.java
+++ src/main/java/za/ac/cput/assignment2/App.java
@@ -14,66 +14,66 @@ public class App
     {
         Collection<String> names = new ArrayList<>();
 
-        names.add("Jason");
-        names.add("George");
-        names.add("Ally");
-        names.add("Rick");
-        names.add("Amber");
+            names.add("Jason");
+            names.add("George");
+            names.add("Ally");
+            names.add("Rick");
+            names.add("Amber");
 
-        names.removeIf(e -> e.startsWith("G"));
+            names.removeIf(e -> e.startsWith("G"));
 
-        System.out.println(names);
+            System.out.println(names);
 
-        if (names.contains("Jason")) {
-            System.out.println("Found Jason");
-        } else {
-            System.out.println("Could not find Jason");
-        }
+            if (names.contains("Jason")) {
+                System.out.println("Found Jason");
+            } else {
+                System.out.println("Could not find Jason");
+            }
 
         Map<Integer,String> map=new HashMap<Integer,String>();
 
-        map.put(1,"Mega");
-        map.put(2,"Jump");
-        map.put(3,"Letter");
-        map.put(4,"Jumbo");
+            map.put(1,"Mega");
+            map.put(2,"Jump");
+            map.put(3,"Letter");
+            map.put(4,"Jumbo");
 
-        map.remove(3);
+            map.remove(3);
 
-        System.out.println(map);
+            System.out.println(map);
 
-        String values = map.get(1);
-        System.out.println(values);
+            String values = map.get(1);
+            System.out.println(values);
 
         Set<String> sets = new HashSet<String>();
 
-        sets.add("Run");
-        sets.add("On");
-        sets.add("The");
-        sets.add("Way");
-        sets.add("To");
+            sets.add("Run");
+            sets.add("On");
+            sets.add("The");
+            sets.add("Way");
+            sets.add("To");
 
-        System.out.println(sets);
+            System.out.println(sets);
 
-        sets.remove("On");
-        System.out.println(sets);
+            sets.remove("On");
+            System.out.println(sets);
 
-        if (sets.contains("Run")) {
-            System.out.println("Found Run");
-        } else {
-            System.out.println("Could not find Run");
-        }
+            if (sets.contains("Run")) {
+                System.out.println("Found Run");
+            } else {
+                System.out.println("Could not find Run");
+            }
 
         List<String> list=new ArrayList<String>();
 
-        list.add("Coffee");
-        list.add("Tea");
-        list.add("Juice");
-        list.add("Soda");
+            list.add("Coffee");
+            list.add("Tea");
+            list.add("Juice");
+            list.add("Soda");
 
-        System.out.println(list);
+            System.out.println(list);
 
-        list.remove(1);
+            list.remove(1);
 
-        System.out.println(list);
+            System.out.println(list);
     }
 }
